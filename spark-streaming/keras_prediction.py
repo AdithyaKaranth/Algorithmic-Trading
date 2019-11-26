9a027062723b7e0f9954a43ae7515abdf5c1cba8
Ubuntu <BigData@BigData.ijojgefqmkvejhgdr4kqlqkxnf.ex.internal.cloudapp.net>
2019-11-26 03:48:33 +0000

    Final Commit

---
+++ spark-streaming/keras_prediction.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from tensorflow.keras.models import model_from_json
+from time import sleep
+import time
+import subprocess
+
+if __name__ == '__main__':
+	json_file = open('model.json', 'r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	# load model architecture
+	loaded_model = model_from_json(loaded_model_json)
+	# load weights into new model
+	loaded_model.load_weights("model.h5")
+	loaded_model.compile(loss="mean_squared_error", optimizer='adam')
+	
+	index = 0
+	curr_len = 0
+	start_time = time.time()
+	predict_num = 0
+	while predict_num<30:
+		directory = os.listdir("out_folder")
+		new_len = len(directory)
+		if new_len > curr_len:
+			file = directory[index]
+			path = "/".join(["out_folder", file, "part-00000"])
+			try:
+				with open(path, "r") as file:
+					for line in file.readlines():
+						data = np.array(float(line)).reshape((1,1,1))
+						pred = loaded_model.predict(data)
+						predict_num += 1
+						print(pred)
+			except:
+				pass
+			finally:	
+				index += 1
+			curr_len = new_len
+
+		
+	elapsed_time = time.time() - start_time
+	print('Elasped time: ' + str(elapsed_time) + '\tPredictions: ' + str(predict_num) + '\tLatency: ' + str(elapsed_time/predict_num))
+
---
+++ spark-streaming/time_series.py
@@ -0,0 +1,31 @@
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.4 pyspark-shell'
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+from tensorflow.keras.models import model_from_json
+import tensorflow as tf
+import numpy as np
+
+def createContext():
+	sc = SparkContext(appName="Kafka2Spark")
+	sc.setLogLevel("ERROR")
+	ssc = StreamingContext(sc, 0.5)
+	
+	# Define KafkaConsumer
+	kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'experiment':1})
+
+	# Processing
+	messages = kafkaStream.map(lambda x: x[1])
+
+	# Write on txt file
+	messages.saveAsTextFiles('file:///home/BigData/Algorithmic-Trading/spark-streaming/out_folder/out.txt')
+	
+	return ssc
+
+if __name__ == '__main__':
+	ssc = StreamingContext.getOrCreate('checkpoint', lambda: createContext())
+	ssc.start()
+	ssc.awaitTermination()
+
